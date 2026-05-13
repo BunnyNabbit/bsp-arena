e5a7a598f58e7d1c852dd8809e7b0069983900e2
BunnyNabbit “Aon” <aon@bunnynabbit.com>
2026-05-13 17:10:45 -0500

    Create cli.mjs

---
+++ cli.mjs
@@ -0,0 +1,18 @@
+// @ts-check
+import { PackedVoxels } from "./class/PackedVoxels.mjs"
+import { ModelBuilder } from "./class/ModelBuilder.mjs"
+import fs from "node:fs"
+import zlib from "node:zlib"
+
+const file = "./example.arena"
+let chunkData = null
+
+{
+	const arenaBuffer = zlib.inflateSync(fs.readFileSync(`${file}`))
+	chunkData = zlib.inflateSync(arenaBuffer.subarray(6, arenaBuffer.length))
+}
+
+const modelBuilder = new ModelBuilder(new PackedVoxels(chunkData))
+console.log("Output length:", modelBuilder.output.length)
+
+fs.writeFileSync("./example.obj", modelBuilder.output)
